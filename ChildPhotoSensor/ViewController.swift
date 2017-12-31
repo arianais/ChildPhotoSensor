b622e0c9d420f91c7d5f0f7ef0668b8c675ba1ab
arianais <arianais@aol.com>
2017-12-31 13:02:24 -0800

    Initial Commit

---
+++ ChildPhotoSensor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ChildPhotoSensor
+//
+//  Created by Ari on 12/31/17.
+//  Copyright © 2017 Logical Nonsense LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
